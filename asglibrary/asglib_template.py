e1fa6b060bb2dcb483bc45f4a76464b2be652bb4
unknown <jwasserzug@WD-ASG30.office.iseoptions.com>
2014-10-03 11:49:50 -0400

    Added ASGLibrary template file

---
+++ asglibrary/asglib_template.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# Author:       <NAME>
+# Created Date: <DATE>
+# Description:  <DESCRIPTION>
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+"""
+INSTRUCTIONS:
+    1. Using your own creativity, create a script that uses at least three
+       keywords/methods from ASGlibrary.
+    2. ASGLibrary Docs: http://asg.ise.com/tap/ASG_Library_Keywords_Docs.html
+"""
+
+# Import required modules here:
+import sys
+# This will allow you to import any module from ASGLibrary
+sys.path.append("/home/bautcar/SVN_TestAutomation/RobotFramework/")
+from ASGLibrary.ASGLibrary import ASGLibrary
+
+
+if __name__ == '__main__':
+    # Instantiate ASGLibrary object
+    # This will allow you to use any method/keyword from ASGLibrary
+    asg_lib = ASGLibrary()
