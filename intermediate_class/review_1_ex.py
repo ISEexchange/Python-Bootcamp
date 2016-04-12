e8f5eea99846dbc6573645e5292e2f4dbb31f046
Carlos Bautista <cbautista@ise.com>
2016-04-12 14:33:18 -0400

    Add review_1_ex.py

---
+++ intermediate_class/review_1_ex.py
@@ -0,0 +1,70 @@
+# 1. Initialize an empty list, list_1
+
+# 2. Initialize another list with 3 strings in it, list_2
+
+# 3. Create a third list, list_3, that has all of list_2's items 4 times. Use
+#    list multiplication
+
+# 4. Using a for loop, copy over list_3's items into list_1
+
+# 5. Verify that list_1's length is equal to list_3 and that list_3's length
+#    is exactly 4 times list_2's length. Use len().
+#        a. Use if/else
+#        b. Use assert
+
+# 6. Use the count function that lists have to verify that each list_3 string
+#    appears 4 times
+#        a. Use one if with 3 clauses connected by and
+#        b. Use one assert with 3 clauses connected by and
+
+# 7. Print every item in list_3 so that the output looks like the below (use
+#    enumerate). First line of output should be for # 1 not # 0. Do this in
+#    two lines of code.
+#        List 3 Item # 1: <>
+#        List 3 Item # 2: <>
+#        List 3 Item # 3: <>
+
+
+# 8. Initialize an empty dictionary, dict_1
+
+# 9. Populate dict_1 with 3 key-value pairs. Values can be anything, but keys:
+#        a. the first key should be an int
+#        b. the second key should be a string
+#        c. the third key should be a tuple of anything
+
+# 10. Store the keys in a new variable, dict_1_keys
+
+# 11. Store the values in a new variable, dict_1_vals
+
+# 12. Zip dict_1_keys and dict_1_vals into a new varible, dict_1_zipped, using
+#     the built in zip() function
+
+# 13. Create a new dictionary, dict_2, out of dict_1_zipped using dict()
+
+# 14. Verify that dict_1 and dict_2 are identical using ==
+
+# 15. Print every key-value pair in dict_1 so that the output looks like the
+#     below. Use enumerate and the dictionary's iteritems() or items(). Do
+#     this in two lines of code.
+#         Dictionary 1 Pair # 0: Key = <>, Value = <>
+#         Dictionary 1 Pair # 1: Key = <>, Value = <>
+#         Dictionary 1 Pair # 2: Key = <>, Value = <>
+
+
+# 16. Assign variables a, b and c to values 1, 2 and 3 in one line using
+#     a. no symbols other than commas and one equal sign
+#     b. tuple unpacking
+#     c. list unpacking
+
+
+# 17. Create a list of words (space separated) from the below string. Use a
+#     function that strings have. Save the list in a variable called words.
+#         some_string = "the big brown fox jumped over the canyon?!"
+
+# 18. Modify the list so that the first letter in the first word is uppercase.
+
+# 19. Modify the list so that the last word is all uppercase.
+
+# 20. Use the string module to see if any words contain any punctuation
+#     characters. If a word contains any punctuation character remove it and save
+#     the result in a new list called clean_words.
