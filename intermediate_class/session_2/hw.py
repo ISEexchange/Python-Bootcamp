d0a6c3d82b77b7b7db26a99fc8db930eef527090
Carlos Bautista <cbautista@ise.com>
2016-04-13 14:52:12 -0400

    Add session 2

---
+++ intermediate_class/session_2/hw.py
@@ -0,0 +1,27 @@
+#################################
+# In Class Assignment and Homework
+#################################
+
+# Thermostat
+
+# Create a Thermostat class to simulate a real life (or not) thermostat.
+# Some internal data members your class should have:
+#     current temperature
+#     degree type (F or C)
+#     user's desired temperature
+#     number of attached air conditioners
+#     number of attached heaters
+#     people detector enabled
+#     AC fan speeds
+#     heater fan speeds
+# Some internal methods your class should have:
+#     turn desired temperature up
+#     turn desired temperatur down
+#     set desired temperature
+#     add an AC
+#     add a heater
+#     remove an AC
+#     remove a heater
+#     auto temp calculation based on people
+#     current temperature display
+#     AC and heater count display
