cce62aba2cca97ed826195ef4313648d9b9c6461
Carlos Bautista <cbautista@ise.com>
2016-04-27 12:54:01 -0400

    Finish session 3 HW

--- intermediate_class/session_3/hw_answered.py
+++ intermediate_class/session_3/hw_answered.py
@@ -80,7 +80,7 @@ class Thermostat(object):
     def _initialize(self):
         """ Set up more complicated initial values """
 
-        print "Starting up '%s' thermostat" % self.name
+        print "Starting up '%s' Thermostat" % self.name
 
         # Pick a random number for initial temperature
         self.current_temp = random.randint(self._lower_bound, self._upper_bound)
@@ -259,24 +259,241 @@ print '############################################################\n\n'
 #     TemperatureChanger objects instead of whatever you had before for tracking
 #     the ACs and heaters.
 
+
+class TemperatureChanger(object):
+    """ """
+
+    def __init__(self):
+        self._on_off = 0
+        self._fan_speed = 0
+
+    def __str__(self):
+        to_print  = 'on/off: %s, ' % self.get_on_off()
+        to_print += 'fanspeed: %s' % self.fan_speed
+
+        return to_print
+
+    def get_on_off(self):
+        """ Getter for on/off """
+        return self._on_off
+
+    def turn_on(self):
+        """ Setter for on/off """
+        print 'Turning fan on'
+        self._on_off = 1
+
+    def turn_off(self):
+        """ Setter for on/off """
+        print 'Turning fan off'
+        self._on_off = 0
+
+    # Using property for fanspeed as an example of how to use properties
+    @property
+    def fan_speed(self):
+        """ """
+        return self._fan_speed
+
+    @fan_speed.setter
+    def fan_speed(self, new_speed):
+        """ """
+        assert new_speed > 0, 'New speed must be greater than 0'
+        assert new_speed < 1, 'New speed must be less than 1'
+
+        self._fan_speed = new_speed
+
+
+class AirConditioner(TemperatureChanger):
+    """ """
+
+    def __init__(self):
+        """ """
+        super(AirConditioner, self).__init__()
+        self.btus = 0
+
+    def __str__(self):
+        """ """
+        to_print = super(AirConditioner, self).__str__()
+        to_print += ', btus: %s' % self.btus
+
+        return to_print
+
+
+class HeatingUnit(TemperatureChanger):
+    """ """
+
+    def __init__(self):
+        """ """
+        super(HeatingUnit, self).__init__()
+        self.watts = 0
+
+    def __str__(self):
+        """ """
+        to_print = super(HeatingUnit, self).__str__()
+        to_print += ', watts: %s' % self.watts
+
+        return to_print
+
+
+# Instead of rewriting the current Thermostat class, I'm going to create a
+# new class for demonstration purposes. This also gives us the flexibility to
+# choose which class to use in our system
+
+class AdvancedThermostat(Thermostat):
+    """ Simulate a real life (or not) thermostat while using
+        AirConditioner and HeatingUnit objects
+    """
+
+    def __init__(self, name):
+
+        # Call the parent class' init method
+        super(AdvancedThermostat, self).__init__(name)
+
+        # Add our own data members
+        self.ac_info     = []
+        self.heater_info = []
+
+        self._adv_initialized()
+
+    def _adv_initialized(self):
+        """ Set up more complicated initial values """
+
+        print "Starting up '%s' AdvancedThermostat" % self.name
+
+        # The parent already set values for data members we don't need
+        # so let's remove them explicitly to clean up the class
+        del self.num_acs
+        del self.num_heaters
+        del self.ac_fan_speeds
+        del self.heater_fan_speeds
+
+        # Lets create some ACs and Heaters
+        self.ac_info = [AirConditioner() for _ in range(3)]
+        self.heater_info = [HeatingUnit() for _ in range(3)]
+
+    def __str__(self):
+        """ Provide a custom string for printing
+            This overrides parent add_AC method
+        """
+
+        to_print = 'Info about the %s AdvancedThermostat\n' % self.name
+        to_print += '  current_temp: %s %s\n'   % (self.current_temp, self.degree_type)
+
+        to_print += '  degree_type: %s\n'       % self.degree_type
+        to_print += '  desired_temp: %s\n'      % self.desired_temp
+        to_print += '  detect_people: %s\n'     % self.detect_people
+
+        for i, ac in enumerate(self.ac_info):
+            to_print += '  ac_info: %s: %s\n'   % (i, ac)
+
+        for i, heater in enumerate(self.heater_info):
+            to_print += '  heater_info: %s: %s\n'   % (i, heater)
+
+        to_print += '\n'
+
+        return to_print
+
+    def add_AC(self):
+        """ Add an AC to be managed by this thermostat
+            This overrides parent add_AC method
+        """
+
+        print 'Adding an AC to %s' % self.name
+
+        self.ac_info.append(AirConditioner())
+
+    def add_heater(self):
+        """ Add a heater to be managed by this thermostat
+            This overrides parent add_heater method
+        """
+
+        print 'Adding a heater to %s' % self.name
+
+        self.heater_info.append(HeatingUnit())
+
+    def remove_AC(self):
+        """ Remove an AC from being managed by this thermostat
+            This overrides parent remove_AC method
+        """
+
+        print 'Removing an AC from %s' % self.name
+        # First check if there is anything to remove
+        if not len(self.ac_info):
+            print 'Nothing to remove'
+            return False
+
+        # Lets remove the oldest AC
+        self.ac_info.pop(0)
+
+    def remove_heater(self):
+        """ Remove a heater from being managed by this thermostat
+            This overrides parent remove_heater method
+        """
+
+        print 'Removing a heater from %s' % self.name
+        # First check if there is anything to remove
+        if not len(self.heater_info):
+            print 'Nothing to remove'
+            return False
+
+        # Lets remove the oldest Heater
+        self.heater_info.pop(0)
+
+    def ac_heater_info(self):
+        """ Print out the AC and heater info
+            This overrides parent ac_heater_info method
+        """
+
+        print 'AC Info'
+        for i, ac in self.ac_info():
+            print 'AC %s: %s' % (i, ac)
+
+        print 'Heater Info'
+        for i, heater in self.heater_info():
+            print 'Heater %s: %s' % (i, heater)
+
+
+adv_thermo_1 = AdvancedThermostat('Advanced Thermostat 1')
+print adv_thermo_1
+adv_thermo_1.add_AC()
+print adv_thermo_1
+
+
 # Comprehensions!
 # Think about what these are going to produce before running it!
 
 # 12. Whats the value of A in:
 #         A = [i for i in range(10, 100, 5)]
 
+# Answer:
+# A = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+
 # 13. Whats the value of B in:
 #         B = [i for i in range(10, 100, 3) if str(i)[0] in ('3','4','5')]
 
+# Answer:
+# B = [31, 34, 37, 40, 43, 46, 49, 52, 55, 58]
+
 # 14. Whats the value of C in:
 #         C = {i for i in [(1,2),(3,4),(5,6),(2,1),'a','b','c']}
+
+# Answer:
+# C = set([(1, 2), 'a', 'c', 'b', (5, 6), (2, 1), (3, 4)])
 
 # 15. Whats the value of D in:
 #         import string
 #         D = {i:i.lower() for i in string.letters.upper()}
 
+# Answer:
+# D = {'A': 'a', 'C': 'c', 'B': 'b', 'E': 'e', 'D': 'd', 'G': 'g', 'F': 'f', 'I': 'i',
+#      'H': 'h', 'K': 'k', 'J': 'j', 'M': 'm', 'L': 'l', 'O': 'o', 'N': 'n', 'Q': 'q',
+#      'P': 'p', 'S': 's', 'R': 'r', 'U': 'u', 'T': 't', 'W': 'w', 'V': 'v', 'Y': 'y',
+#      'X': 'x', 'Z': 'z'}
+
 # 16. Whats the value of E in:
 #         E = [i for i in A for j in B if i == j]
+
+# Answer:
+# E = [40, 55]
 
 # 17. Change the following function so a list comprehension is used
 #
@@ -290,5 +507,30 @@ print '############################################################\n\n'
 #                     return False
 #             return True
 
+from string import lowercase, digits
+str_to_process = 'O329u@T4Gs5-HH643.hf838'
+allowed_chars = lowercase + digits
+
+
+def checker(str_to_process):
+    """ """
+    result = [False for c in str_to_process if c not in allowed_chars]
+    if False in result:
+        return False
+    else:
+        return True
+
+print checker(str_to_process)  # returns False
+print checker('kjdkf92932j2nnfdsjfisda92ik')  # returns True
+
+
 # 18. If its not already, update checker() from above so that the body
 #     is only one line of code
+
+
+def checker(str_to_process):
+    """ """
+    return all([False for c in str_to_process if c not in allowed_chars])
+
+print checker(str_to_process)  # returns False
+print checker('kjdkf92932j2nnfdsjfisda92ik')  # returns True
