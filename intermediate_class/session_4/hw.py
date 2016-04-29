194e7dc0a45044055f23022263a7138894f4d560
Carlos Bautista <cbautista@ise.com>
2016-04-29 16:04:44 -0400

    Add session 4

---
+++ intermediate_class/session_4/hw.py
@@ -0,0 +1,31 @@
+#################################
+# In Class Assignment and Homework
+#################################
+
+# 1. Create a class called Packet that contains:
+#        - header  - integer
+#        - data  - integer
+#        - trailer  - integer
+#        - fill_in_values() - function
+#
+# 2. Create a class called PacketGenerator that contains:
+#        - generate_packets() - function generator
+#            - this function yields a packet object on request
+#
+# 3. In main, instantiate a PacketGenerator. Generate packets in a while loop.
+#    Print the contents of the packet
+
+
+
+
+
+
+# bret
+# rob
+# shray
+# girish
+# andrew
+# jason
+# priyanka
+# sef
+# han
