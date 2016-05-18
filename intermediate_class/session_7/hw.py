387d6826c0c7570094541ec960bf82263802a03b
Carlos Bautista <cbautista@ise.com>
2016-05-18 15:02:23 -0400

    Add session 7

---
+++ intermediate_class/session_7/hw.py
@@ -0,0 +1,10 @@
+#################################
+# In Class Assignment and Homework
+#################################
+
+# 1. Lets say there is a web server log file that contains content like that
+#    shown in log1.txt. Create a python script that can provide stats such as:
+#    a. What is the most popular URL/file?
+#    b. How often do URLs return a 304 vs 200 vs etc...?
+#    c. What is the average latency for a specific URL?
+#    d. Errors start with [1]. Does the file contain any?
